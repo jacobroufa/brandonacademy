0c148a8aea6bc4ebf49be617e60d4a834327f770
Jacob Roufa <jacob.roufa@gmail.com>
2012-04-12 17:50:04 -0700

    Update script.js to use Drupal behaviors

--- js/script.js
+++ js/script.js
@@ -1,40 +1,54 @@
-jQuery(document).ready(function($) {
+(function($) {
 
 // Reset the search form
-  $("#edit-sitewide").blur();
+  Drupal.behaviors.baBlur = {
+    attach: function (context, settings) {
+      $('#edit-sitewide', context).blur();
+    }
+  };
 
 // Sets up the list navigation on /discover
-  $("#discoverList").listnav({ 
-    includeAll: false,  
-    includeNums: true,
-    noMatchText: 'There are no artists in our database whose names begin with that letter.',
-    showCounts: false,
-    prefixes: ['the','a']
-  });
+  Drupal.behaviors.baDiscoverLN = {
+    attach: function (context, settings) {
+      $('#discoverList', context).listnav({ 
+        includeAll: false,  
+        includeNums: true,
+        noMatchText: 'There are no artists in our database whose names begin with that letter.',
+        showCounts: false,
+        prefixes: ['the','a']
+      });
+    }
+  };
 
 // Position the Feedback link element
-  //$("#block-feedback-form .feedback-link").prependTo("footer");
-  $("body.node-type-article #block-feedback-form .feedback-link").text('Broken video?').prependTo("article .below-content");
+  Drupal.behaviors.feedbackLink = {
+    attach: function (context, settings) {
+      $('body.node-type-article #block-feedback-form .feedback-link', context).text('Broken video?').prependTo('article .below-content');
+    }
+  };
 
 // Front Page Recent Tutorials Carousel
-//  $("#recent-tutorials .recent-tutorials").carousel('#previous', '#next');
-  function recentCarousel(carousel) {
-    jQuery('#next').bind('click', function() {
-      carousel.next();
-      return false;
-    });
-    jQuery('#previous').bind('click', function() {
-      carousel.prev();
-      return false;
-    });
-  }; 
+  Drupal.behaviors.recentTutorials = {
+    attach: function (context, settings) {
+      $('#recent-tutorials .recent-tutorials', context).jcarousel({
+        wrap: 'circular',
+        scroll: 1,
+        initCallback: recentCarousel,
+        buttonNextHTML: null,
+        buttonPreviousHTML: null,
+      });
 
-  $("#recent-tutorials .recent-tutorials").jcarousel({
-    wrap: 'circular',
-    scroll: 1,
-    initCallback: recentCarousel,
-    buttonNextHTML: null,
-    buttonPreviousHTML: null,
-  });
+      function recentCarousel(carousel) {
+        $('#next', context).bind('click', function() {
+          carousel.next();
+          return false;
+        });
+        $('#previous', context).bind('click', function() {
+          carousel.prev();
+          return false;
+        });
+      }; 
+    }
+  };
 
-});
+})(jQuery);
