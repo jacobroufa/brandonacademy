78ffd6c105a704a9b739c60409957c1a17ff2daa
Jacob Roufa <jacob.roufa@gmail.com>
2012-04-13 23:28:02 -0700

    Update node.tpl.php to remove author and date

--- tpl/node.tpl.php
+++ tpl/node.tpl.php
@@ -20,6 +20,8 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+      hide($content['created']);
+      hide($content['author']);
       print render($content);
     ?>
   </div>
